ed491aba6f2792f15d9d0ee2a42f8eb7c1b1be45
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-09-10 12:18:30 +0900

    lettuce data

---
+++ R/data.R
@@ -0,0 +1,9 @@
+
+#' Lettuce trial
+#'
+#' @source https://figshare.com/articles/dataset/Lettuce_trial_phenotypic_and_marker_data_/8299493
+#' @rdname lettuce
+"lettuce_phenotypes"
+
+#' @rdname lettuce
+"lettuce_markers"
