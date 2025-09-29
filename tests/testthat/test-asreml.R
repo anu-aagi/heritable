c5889fb381f8e55ad840b094fcc5a0526fedc568
Fonti Kar <fonti.kar@gmail.com>
2025-09-29 11:41:24 +1000

    Build ignore README outputs, added asreml and agridat dependency in DESCRIPTION

--- tests/testthat/test-asreml.R
+++ tests/testthat/test-asreml.R
@@ -1,9 +1,11 @@
 test_that("heritability for asreml works", {
+  skip_if_not_installed("asreml")
+  skip_on_cran()
 
   fit <- asreml::asreml(yield ~ rep,
-                        random = ~gen + rep:block,
-                        data = agridat::john.alpha)
+    random = ~ gen + rep:block,
+    data = agridat::john.alpha
+  )
 
   expect_equal(H2(fit, target = "gen"), 0.8090841)
-
 })
