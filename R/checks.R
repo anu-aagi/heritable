17a5fa7a569a951be76c094f23e9d2e6e99712ac
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-11-14 12:06:47 +1100

    target NULL check moved to target_single check

--- R/checks.R
+++ R/checks.R
@@ -1,7 +1,8 @@
-
-
 initial_checks <- function(model, target, options) {
-  if(options$check %||% TRUE) {
+  if (options$check %||% TRUE) {
+    # If there is more than one target, error
+    check_target_single(target)
+
     # If model has not converged, warn
     check_model_convergence(model)
 
@@ -13,21 +14,23 @@ initial_checks <- function(model, target, options) {
 # Check if model converged
 #' @keywords internal
 check_model_convergence.asreml <- function(model) {
-    if (!model$converge) {
-        cli::cli_warn("The input model has not converged, estimates may be unreliable")
-    }
+  if (!model$converge) {
+    cli::cli_warn(
+      "The input model has not converged, estimates may be unreliable"
+    )
+  }
 }
 
 #' @keywords internal
 check_model_convergence.lmerMod <- function(model) {
-    if (model@optinfo$conv$opt != 0) {
-        warning("The input model has not converged")
-    }
+  if (model@optinfo$conv$opt != 0) {
+    warning("The input model has not converged")
+  }
 }
 
 #' @keywords internal
 check_model_convergence <- function(model) {
-    UseMethod("check_model_convergence")
+  UseMethod("check_model_convergence")
 }
 .S3method("check_model_convergence", "asreml", check_model_convergence.asreml)
 .S3method("check_model_convergence", "lmerMod", check_model_convergence.lmerMod)
@@ -37,42 +40,49 @@ check_model_convergence <- function(model) {
 # Check if only one target has been supplied
 #' @keywords internal
 check_target_single <- function(target) {
-    if (length(target) > 1) {
-        cli::cli_abort("Only one target can be supplied to calculate heritability")
-    }
+  if (is.null(target)) {
+    cli::cli_abort(
+      "The target is {.value NULL}. Please specify the target name."
+    )
+  }
+  if (length(target) > 1) {
+    cli::cli_abort("Only one target can be supplied to calculate heritability")
+  }
 }
 
 # Check if target is in model
 #' @keywords internal
 check_target_exists <- function(model, target) {
-    model_terms <- pull_terms(model)
-    if(is.null(target)) {
-      cli::cli_abort("The target is {.value NULL}. Please specify the target name.")
-    }
-    if (!target %in% c(model_terms$fixed, model_terms$random)) {
-        cli::cli_abort("The specified target {.code {target}} is not found in the model")
-    }
+  model_terms <- pull_terms(model)
+  if (!target %in% c(model_terms$fixed, model_terms$random)) {
+    cli::cli_abort(
+      "The specified target {.code {target}} is not found in the model"
+    )
+  }
 }
 
 # Check if target is in fixed or random
 #' @keywords internal
 check_target_random <- function(model, target) {
-    model_terms <- pull_terms(model)
-    if (target %in% model_terms$random) {
-        TRUE
-    } else {
-        FALSE
-    }
+  model_terms <- pull_terms(model)
+  if (target %in% model_terms$random) {
+    TRUE
+  } else {
+    FALSE
+  }
 }
 
 # Check if target is in both fixed and random
 #' @keywords internal
 check_target_both <- function(model, target) {
-    model_terms <- pull_terms(model)
-    # Use regex to check for presence in both fixed and random
-    if (any(grepl(target, model_terms$fixed, fixed = TRUE)) && any(grepl(target, model_terms$random, fixed = TRUE))) {
-        TRUE
-    } else {
-        FALSE
-    }
+  model_terms <- pull_terms(model)
+  # Use regex to check for presence in both fixed and random
+  if (
+    any(grepl(target, model_terms$fixed, fixed = TRUE)) &&
+      any(grepl(target, model_terms$random, fixed = TRUE))
+  ) {
+    TRUE
+  } else {
+    FALSE
+  }
 }
