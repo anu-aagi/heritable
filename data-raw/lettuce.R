6e6fb509ddbb22f384005706540b7e91cc86e405
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-11-15 13:42:13 +1100

    udpate data

--- data-raw/lettuce.R
+++ data-raw/lettuce.R
@@ -1,7 +1,23 @@
 ## code to prepare `lettuce` dataset goes here
 library(tidyverse)
-lettuce_phenotypes <- read_delim("data-raw/lettuce-data/phenotypes_lettuce.txt")
-lettuce_markers <- read_delim("data-raw/lettuce-data/markers_lettuce.txt")
+lettuce_phenotypes <- read_delim("data-raw/lettuce-data/phenotypes_lettuce.txt",
+                                 col_types = cols(
+                                   loc = col_factor(),
+                                   gen = col_factor(),
+                                   rep = col_factor(),
+                                   y = col_double()
+                                 )) |>
+  # get rid of attributes
+  as.data.frame() |>
+  as_tibble()
+str(lettuce_phenotypes)
+
+lettuce_markers <- read_delim("data-raw/lettuce-data/markers_lettuce.txt") |>
+  # get rid of attributes
+  as.data.frame() |>
+  as_tibble()
+
+str(lettuce_markers)
 
 usethis::use_data(lettuce_phenotypes, overwrite = TRUE)
 usethis::use_data(lettuce_markers, overwrite = TRUE)
