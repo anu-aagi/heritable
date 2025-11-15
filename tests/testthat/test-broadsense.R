6d2114b8e72a8dd1c2b850a2986cc7a89ed5e4c0
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-11-15 11:58:58 +1100

    update broadsense test

--- tests/testthat/test-broadsense.R
+++ tests/testthat/test-broadsense.R
@@ -4,67 +4,50 @@ test_that("heritability for asreml works", {
 
 
 # random gen --------------------------------------------------------------
-  truth <-
-    c(
-      "Cullis" = 0.8090841,
-      "Oakey" = 0.8090728,
-      "Piepho" = 0.8029759,
-      "Delta" = 0.8397128, # CHECK VALUE
-      "Naive" = 0.6364751 # CHECK VALUE
-    )
+  truth_random_asreml <- c(
+    "Cullis" = 0.8090841,
+    "Oakey" = 0.8090728,
+    "Piepho" = 0.8029759,
+    "Delta" = 0.8090841, # arithmetic mean or harmonic mean?
+    "Naive" = 0.8400648
+  )
 
   model <- asreml::asreml(yield ~ rep,
                           random = ~ gen + rep:block,
                           data = agridat::john.alpha,
                           trace = FALSE)
-  expect_equal(H2(model, target = "gen"), truth, tolerance = 1e-7)
+  expect_equal(H2(model, target = "gen"), truth_random_asreml, tolerance = 1e-7)
+
+  # NOTE: in theory these values should be the same
+  # differences seem to arise because of different estimates of VCOMP
+  truth_random_lme4 <- c(
+    "Cullis" = 0.8091339,
+    "Oakey" = 0.8091339,
+    "Piepho" = 0.7966376,
+    "Delta" = truth_asreml[["Delta"]],
+    "Naive" = truth_asreml[["Naive"]]
+  )
 
   model <- lme4::lmer(data = dat, formula = yield ~ rep + (1 | gen) + (1 | rep:block))
-  expect_equal(H2(model, target = "gen"), truth, tolerance = 1e-7)
+  expect_equal(H2(model, target = "gen"), truth_random_lme4, tolerance = 1e-4)
 
   # fixed gen ---------------------------------------------------------------
-  truth <- c("Cullis" = NA,
-             "Oakey" =  NA,
-             "Piepho" = 0.8029759,
-             "Delta" = 0.8030227, # CHECK VALUE
-             "Naive" = NA)
+  truth_fixed_asreml <- c(
+    "Cullis" = NA,
+    "Oakey" = NA,
+    "Piepho" = truth_random_asreml[["Piepho"]],
+    "Delta" = 0.8030227,
+    "Naive" = NA
+  )
 
   model <- asreml::asreml(yield ~ rep + gen,
                           random = ~ rep:block,
                           data = agridat::john.alpha,
                           trace = FALSE
   )
-  expect_equal(H2(model, target = "gen"), truth, tolerance = 1e-7)
+  expect_equal(H2(model, target = "gen"), truth_fixed_asreml, tolerance = 1e-5)
 
   model <- lme4::lmer(data = dat, formula = yield ~ rep + gen + (1 | rep:block))
   expect_equal(H2(model, target = "gen"), truth, tolerance = 1e-7)
-
-})
-
-test_that("counterpart model can be fitted", {
-  skip_if_not_installed("asreml")
-  skip_on_cran()
-
-  fit <- asreml::asreml(yield ~ rep,
-    random = ~ gen + rep:block,
-    data = agridat::john.alpha,
-    trace = FALSE
-  )
-
-  fit_counter <- fit_counterpart_model.asreml(fit, target = "gen")
-
-  expect_true("gen" %in% pull_terms(fit_counter)$fixed)
-  expect_false("gen" %in% pull_terms(fit_counter)$random)
-
-   fit_fixed <- asreml::asreml(yield ~ rep + gen,
-    random = ~ rep:block,
-    data = agridat::john.alpha,
-    trace = FALSE
-  )
-
-  fit_counter <- fit_counterpart_model.asreml(fit_fixed, target = "gen")
-
-  expect_false("gen" %in% pull_terms(fit_counter)$fixed)
-  expect_true("gen" %in% pull_terms(fit_counter)$random)
 
 })
