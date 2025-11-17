7fea402cae1e51ea43e259be45bb2d972fa822de
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-11-17 14:44:18 +1100

    vctrs compatability

---
+++ R/vctrs.R
@@ -0,0 +1,21 @@
+
+
+#' @importFrom vctrs vec_ptype2
+#' @export
+vec_ptype2.heritable.heritable <- function(x, y, ...) double()
+
+#' @export
+vec_ptype2.heritable.double <- function(x, y, ...) double()
+
+#' @export
+vec_ptype2.double.heritable <- function(x, y, ...) double()
+
+#' @importFrom vctrs vec_cast
+#' @export
+vec_cast.heritable.heritable <- function(x, to, ...) x
+
+#' @export
+vec_cast.double.heritable <- function(x, to, ...) x
+
+#' @export
+vec_cast.heritable.double <- function(x, to, ...) x
