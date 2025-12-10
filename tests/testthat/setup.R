61e6241ae94855c1596c2843e9124e26f791d60e
Fonti Kar <fonti.kar@gmail.com>
2025-12-11 10:03:52 +1100

    Adding test fixtures and cleaning up #17

---
+++ tests/testthat/setup.R
@@ -0,0 +1,71 @@
+# # ASREML
+# # Genotype as random effect
+# asreml_model_random <- asreml::asreml(
+#   fixed = yield ~ rep,
+#   random = ~ gen + rep:block,
+#   data = agridat::john.alpha,
+#   trace = FALSE
+# )
+#
+# saveRDS(asreml_model_random, file = test_path("fixtures/asreml_model_random.rds"))
+#
+# # Genotype as fixed effect
+# asreml_model_fixed <- asreml::asreml(
+#   fixed = yield ~ rep + gen,
+#   random = ~ rep:block,
+#   data = agridat::john.alpha,
+#   trace = FALSE
+# )
+#
+# saveRDS(asreml_model_fixed, file = test_path("fixtures/asreml_model_fixed.rds"))
+#
+# # Genotype as both fixed and random effect
+# asreml_model_both <- asreml::asreml(
+#   fixed = yield ~ gen,
+#   random = ~ gen:block,
+#   data = agridat::john.alpha,
+#   trace = FALSE
+# )
+#
+# saveRDS(asreml_model_both, file = test_path("fixtures/asreml_model_both.rds"))
+#
+# # # Model that is not convegd
+# # asreml_model_failed_converge <- asreml_model_random
+# # model_failed_converge$converge <- FALSE
+#
+# # GxE models --------------------------------------------------------------
+# asremL_model_g_by_e <- asreml::asreml(yield ~ year:loc:trial,
+#                         random = ~ gen + gen:loc,
+#                         data = agridat::adugna.sorghum,
+#                         trace = FALSE
+# )
+#
+# saveRDS(asremL_model_g_by_e, file = test_path("fixtures/asreml_model_g_by_e.rds"))
+#
+# # Narrow sense
+# asreml_model_grm <- asreml::asreml(y ~ rep,
+#                         random=~ vm(gen, lettuce_GRM),
+#                         data = lettuce_phenotypes |>
+#                           subset(loc == "L2"))
+#
+# saveRDS(asreml_model_grm, file = test_path("fixtures/asreml_model_grm.rds"))
+#
+# # lme4
+#
+# # Genotype as random effect
+# lmer_model_random <- lme4::lmer(data = agridat::john.alpha, formula = yield ~ rep + (1 | gen) + (1 | rep:block))
+#
+# saveRDS(lmer_model_random, file = test_path("fixtures/lmer_model_random.rds"))
+#
+# # Genotype as fixed effect
+# lmer_model_fixed <- lme4::lmer(data = agridat::john.alpha, formula = yield ~ rep + gen + (1 | rep:block))
+#
+# saveRDS(lmer_model_fixed, file = test_path("fixtures/lmer_model_fixed.rds"))
+#
+# # Genotype as both fixed and random effect
+# lmer_model_g_by_e <- lme4::lmer(data = agridat::adugna.sorghum, formula = yield ~ year:loc:trial + (1 | gen) + (1 | gen:loc))
+#
+# saveRDS(lmer_model_g_by_e, file = test_path("fixtures/lmer_model_g_by_e.rds"))
+#
+# #TODO: Narrow sense for lme4
+#
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,58 +1,32 @@
+test_that("Helper functions working with asreml", {
+  skip_if_not_installed("asreml")
+  skip_on_cran()
+
+  asreml_model_random <- readRDS(test_path("fixtures/asreml_model_random.rds"))
+  asreml_model_fixed <- fit_counterpart_model(asreml_model_random, target = "gen")
+
+  # Can we pull out the correct terms?
+  # Can we fit the counter model?
+
+  expect_named(pull_terms(asreml_model_random), c("fixed", "random"))
+  expect_true("gen" %in% pull_terms(asreml_model_random)$random)
+  expect_true("gen" %in% pull_terms(asreml_model_fixed)$fixed)
+})
+
+
 test_that("Helper functions working with lme4", {
   skip_on_cran()
   skip_if_not_installed("lme4")
 
-  dat <- agridat::john.alpha
-  target <- "gen"
+  lmer_model_random <- readRDS(test_path("fixtures/lmer_model_random.rds"))
+  lmer_model_fixed <- fit_counterpart_model(lmer_model_random, target = "gen")
 
-  model_ran_lmer <- lme4::lmer(data = dat, formula = yield ~ rep + (1 | gen) + (1 | rep:block))
-  model_fixed_lmer <- lme4::lmer(data = dat, formula = yield ~ rep + gen + (1 | rep:block))
-
-  # Pull out the terms correctly?
-  model_ran_lmer_terms <- pull_terms(model_ran_lmer)
-
-  expect_named(model_ran_lmer_terms, c("fixed", "random"))
-
-  # Fit counterpart models?
-  model_lmer_counter <- fit_counterpart_model.lmerMod(model_ran_lmer, target = "gen")
-  model_lmer_fixed_counter <- fit_counterpart_model.lmerMod(model_fixed_lmer, target = "gen")
-
-  expect_true(any(grepl(target, pull_terms(model_lmer_counter)$fixed)))
-  expect_true(any(grepl(target, pull_terms(model_lmer_fixed_counter)$random)))
+  # Can we pull out the correct terms?
+  # Can we fit the counter model
+  expect_named(pull_terms(lmer_model_random), c("fixed", "random"))
+  expect_true("gen" %in% pull_terms(lmer_model_random)$random)
+  expect_true("gen" %in% pull_terms(lmer_model_fixed)$fixed)
 })
 
 
-test_that("Helper functions working with asreml", {
-  skip_on_cran()
-  skip_if_not_installed("asreml")
 
-  dat <- agridat::john.alpha
-  target <- "gen"
-
-  # random genotype effect
-  model_ran_asreml <- asreml::asreml(
-    data = dat,
-    fixed = yield ~ rep,
-    random = ~ gen + rep:block,
-    trace = FALSE
-  )
-
-  model_fixed_asreml <- asreml::asreml(
-    data = dat,
-    fixed = yield ~ rep + gen,
-    random = ~ rep:block,
-    trace = FALSE
-  )
-
-  # can it pull out the correct terms?
-  model_ran_asreml_terms <- pull_terms(model_ran_asreml)
-
-  expect_named(model_ran_asreml_terms, c("fixed", "random"))
-
-  # can it fit counterpart models?
-  model_asreml_counter <- fit_counterpart_model.asreml(model_ran_asreml, target = "gen")
-  model_asreml_fixed_counter <- fit_counterpart_model.asreml(model_fixed_asreml, target = "gen")
-
-  expect_true(any(grepl(target, pull_terms(model_asreml_counter)$fixed)))
-  expect_true(any(grepl(target, pull_terms(model_asreml_fixed_counter)$random)))
-})
