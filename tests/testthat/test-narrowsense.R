c36fec449b8ce017c72335897b252c14e87e5a3a
Fonti Kar <fonti.kar@gmail.com>
2025-12-16 09:10:02 +1100

    Addressing meta-data fixes  #22

--- tests/testthat/test-narrowsense.R
+++ tests/testthat/test-narrowsense.R
@@ -23,4 +23,7 @@ test_that("h2 heritability works", {
 
   expect_named(h2(asreml_model_grm, target = "gen"), c("Oakey", "Delta"))
   expect_length(h2(asreml_model_grm, target = "gen"), 2)
+
+  expect_named(h2_Delta_by_genotype(asreml_model_grm, target = "gen", type = "BLUP"), levels(asreml_model_grm$mf$gen), ignore.order = TRUE)
+  expect_equal(nrow(h2_Delta_pairwise(asreml_model_grm, target = "gen", type = "BLUP")), length(levels(asreml_model_grm$mf$gen)))
 })
