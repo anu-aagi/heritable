d257e40879ff888f530679c681353ff506769c84
Fonti Kar <fonti.kar@gmail.com>
2025-12-16 14:51:47 +1100

    Unexported h2 for now #16

--- R/broad-sense.R
+++ R/broad-sense.R
@@ -1,3 +1,43 @@
+#' Calculate heritability from mixed model object
+#' @description
+#' A case-specific wrapper for calculating heritability.
+#'
+# - The lowercase prefix `h2_` refers to the wrapper or subfunctions e.g. [`h2_Oakey()`] for calculating narrow sense heritability
+#' - The upper case prefix `H2_` refers to the wrapper or subfunctions e.g. [`H2_Delta()`] for calculating broad sense heritability
+#' @param model Model object of class `lmerMod/merMod` or `asreml`
+#' @param method Character vector of name of method to calculate heritability. See details.
+#' @param target The name of the random effect for which heritability is to be calculated.
+#' @param options NULL by default, for internal checking of model object before calculations
+#' @aliases H2
+#' @usage
+# h2(model, target, method = c("Oakey", "Delta"), options)
+#' H2(model, target, method = c("Cullis", "Oakey", "Delta", "Piepho", "Standard"), options)
+#' @returns A named numeric vector, length matching number of methods supplied
+#' @details
+#'
+# The following methods are currently implemented for narrow-sense heritability `h2(method = "XX")`:
+#
+# - `"Oakey"`: \deqn{H^2_{Oakey} = \frac{\sum_{i = n_z+1}^{n_g} \lambda_i}{\sum_{n_g}^{\lambda_i\neq 0}}}
+# - `"Delta"`: \deqn{H^2_{\Delta ..} = 1 - \frac{PEV^{BLUP}_{\overline\Delta ..}}{2\sigma^2_g}}
+#'
+#' The following methods are currently implemented for broad-sense heritability `H2(method = "XX")`:
+#'
+#' - `"Cullis"`: \deqn{H^2_{Cullis} = 1 - \frac{PEV^{BLUP}_{\overline\Delta ij}}{2\sigma^2_g}}
+#' - `"Oakey"`: \deqn{H^2_{Oakey} = \frac{\sum_{i = n_z+1}^{n_g} \lambda_i}{\sum_{n_g}^{\lambda_i\neq 0}}}
+#' - `"Delta"`: \deqn{H^2_{\Delta ..} = 1 - \frac{PEV^{BLUP}_{\overline\Delta ..}}{2\sigma^2_g}}
+#' - `"Piepho"`: \deqn{H^2_{Piepho} = \frac{\sigma^2_g}{\sigma^2_g + \overline{PEV_{BLUE_g}} / 2}}
+#' - `"Standard"`: \deqn{H^2_{Standard} = \frac{\sigma^2_g}{\sigma^2_g + \frac{1}{n_g}\sum_{n_g}^{i=1} \sigma^2_p / n_{gi}}}
+#'
+#' For further details of a specific method - take a look at help file for each subfunctions `?H2_Cullis`
+#'
+#' @references
+#' - Cullis, B. R., Smith, A. B., & Coombes, N. E. (2006). On the design of early generation variety trials with correlated data. Journal of Agricultural, Biological, and Environmental Statistics, 11(4), 381–393. https://doi.org/10.1198/108571106X154443
+#' - Oakey, H., Verbyla, A., Pitchford, W., Cullis, B., & Kuchel, H. (2006). Joint modeling of additive and non-additive genetic line effects in single field trials. Theoretical and Applied Genetics, 113(5), 809–819. https://doi.org/10.1007/s00122-006-0333-z
+#' - Schmidt, P., Hartung, J., Rath, J., & Piepho, H.-P. (2019). Estimating Broad-Sense Heritability with Unbalanced Data from Agricultural Cultivar Trials. Crop Science, 59(2), 525–536. https://doi.org/10.2135/cropsci2018.06.0376
+#' - Piepho, H.-P., & Möhring, J. (2007). Computing Heritability and Selection Response From Unbalanced Plant Breeding Trials. Genetics, 177(3), 1881–1888. https://doi.org/10.1534/genetics.107.074229
+#' - Falconer, D. S., & Mackay, T. F. C. (1996). Introduction to quantitative genetics (4th ed.). Longman.
+#' @seealso [H2_Cullis()], [H2_Oakey()], [H2_Delta()], [H2_Piepho()], [H2_Standard()]
+# [`h2_Oakey()`], [`h2_Delta()`]
 #' @export
 H2 <- function(model,
                target = NULL,
@@ -42,7 +82,7 @@ H2.default <- function(model,
 #' @description Compute standard heritability using the classic ratio method of
 #' genotypic and phenotypic variance. See Falconer & Mackay (1996)
 #' @usage H2_Standard(model, target, options)
-#' @inheritParams h2
+#' @inheritParams H2
 #' @return Single numeric value
 #' @details
 #' The equation used to calculate standard heritability is:
@@ -54,7 +94,7 @@ H2.default <- function(model,
 #' - \eqn{\sigma_g} is the variance attributed to genotype differences
 #' - \eqn{\sigma_p} is the variance attributed to phenotypic differences
 #' @export
-#' @references @references
+#' @references
 #' Falconer, D. S., & Mackay, T. F. C. (1996). Introduction to quantitative genetics (4th ed.). Longman.
 H2_Standard <- function(model, target, options) {
   UseMethod("H2_Standard")
@@ -63,7 +103,7 @@ H2_Standard <- function(model, target, options) {
 #' Calculate Cullis' heritability from model object
 #' @description Compute "generalised heritability" for unbalanced experimental designs.
 #' See Cullis, Smith and Coombes (2006) for derivation.
-#' @inheritParams h2
+#' @inheritParams H2
 #' @usage H2_Cullis(model, target, options)
 #' @return Single numeric value
 #' @details The equation for Cullis heritability is as follow
@@ -89,7 +129,7 @@ H2_Oakey <- function(model, target, options) {
 #' Calculate Piepho's heritability from model object
 #' Compute Piepho's heritability using variance differences between genotype BLUEs
 #' @usage H2_Piepho(model, target, options)
-#' @inheritParams h2
+#' @inheritParams H2
 #' @details The equation for Piepho's heritability is as follows:
 #'
 #' \deqn{H^2_{Piepho} = \frac{\sigma^2_g}{\sigma^2_g + \overline{PEV_{BLUE_g}} / 2}}
