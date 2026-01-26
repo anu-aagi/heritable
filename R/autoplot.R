354c9a3870e7d62ba3c54e990299a1d6825ba6da
Fonti Kar <fonti.kar@gmail.com>
2026-01-27 10:38:51 +1100

    #39 Autoplot implemented with few plot types

--- R/autoplot.R
+++ R/autoplot.R
@@ -1,96 +1,113 @@
-# @export
+#' Extract variance component values method
+#' @export
+extract_var_comps <- function(model){
+  UseMethod("extract_var_comps")
+}
+
+#' Extract variance component values from asreml model object
+#' @noRd
+#' @keywords internal
+#' @export
 extract_var_comps.asreml <- function(model) {
-  tibble(
+  dplyr::tibble(
     component = names(model$vparameters),
     value     = model$vparameters
   ) |>
-    mutate(
+    dplyr::mutate(
       percent = value / sum(value) * 100
-    ) |>
-    structure(class = c("var_comps", "tbl_df", "tbl"))
-}
-
-# Bar plot
-  bar_plot <- function(var_comps){
-  ggplot(var_comps, aes(x = component, y = value)) +
-    geom_bar(stat = "identity") +
-    labs(title = "Variance Components", x = "Component", y = "Value") +
-    theme_minimal()
-  }
-
-  # Stacked bar plot
-  stacked_bar_plot <- function(var_comps){
-  ggplot(var_comps, aes(x = "", y = value, fill = component)) +
-    geom_bar(stat = "identity", width = 1) +
-    geom_text(aes(label = paste0(round(percent, 1), "%")), 
-              position = position_stack(vjust = 0.5)) +
-    theme_minimal() +
-    theme(
-      axis.title = element_blank(),
-      axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        panel.grid  = element_blank()
-        ) 
-  }
-
-  # Pie plot
-  pie_plot <- function(var_comps){
-  ggplot(var_comps, aes(x = "", y = value, fill = component)) +
-    geom_bar(stat = "identity", width = 1) +
-    coord_polar("y", start = 0) +
-    geom_text(aes(label = paste0(round(percent, 1), "%")), 
-              position = position_stack(vjust = 0.5)) +
-    theme_minimal() +
-    theme(
-      axis.title = element_blank(),
-      axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        panel.grid  = element_blank()
-        )
-  }
-    # Donut plot
-    donut_plot <- function(var_comps) {
-        df <- var_comps |>
-            mutate(
-                fraction = percent / 100,
-                ymax = cumsum(fraction),
-                ymin = lag(ymax, default = 0)
-            )
-
-        ggplot(df, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = component)) +
-            geom_rect() +
-            coord_polar(theta = "y") +
-            xlim(2, 4) +
-            geom_label(
-                aes(
-                    x = 3.5,
-                    y = (ymin + ymax) / 2,
-                    label = paste0(round(percent, 1), "%")
-                ),
-                fill = NA,
-                label.size = 0
-            ) +
-            theme_void()
-            }
-
-plot_var_comps <- function(var_comps, type = c("bar", "stacked", "pie", "donut")) {
-    type <- match.arg(type)
-
-    switch(type,
-        bar = bar_plot(var_comps),
-        stacked = stacked_bar_plot(var_comps),
-        pie = pie_plot(var_comps),
-        donut = donut_plot(var_comps)
     )
 }
 
-#' export
+#' Bar plot
+#' @noRd
+#' @keywords internal
+#' @importFrom ggplot2 ggplot aes geom_bar theme_minimal
+bar_plot <- function(var_comps){
+  ggplot(var_comps, aes(x = component, y = value, fill = component)) +
+    geom_bar(stat = "identity") +
+    theme_minimal()
+}
+
+#' Stacked bar plot
+#' @noRd
+#' @keywords internal
+#' @importFrom ggplot2 ggplot aes geom_bar geom_text theme_minimal theme element_blank position_stack
+stacked_bar_plot <- function(var_comps){
+  ggplot(var_comps, aes(x = "", y = value, fill = component)) +
+    geom_bar(stat = "identity", width = 1) +
+    geom_text(aes(label = paste0(round(percent, 1), "%")),
+              position = position_stack(vjust = 0.5)) +
+    theme_minimal() +
+    theme(
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid  = element_blank()
+    )
+}
+
+#' Pie plot
+#' @noRd
+#' @keywords internal
+#' @importFrom ggplot2 ggplot aes geom_bar coord_polar geom_text theme_minimal theme element_blank position_stack
+pie_plot <- function(var_comps){
+  ggplot(var_comps, aes(x = "", y = value, fill = component)) +
+    geom_bar(stat = "identity", width = 1) +
+    coord_polar("y", start = 0) +
+    geom_text(aes(label = paste0(round(percent, 1), "%")),
+              position = position_stack(vjust = 0.5)) +
+    theme_minimal() +
+    theme(
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      panel.grid  = element_blank()
+    )
+}
+
+#' Donut plot
+#' @noRd
+#' @keywords internal
+#' @importFrom ggplot2 ggplot aes geom_rect coord_polar geom_label theme_void xlim
+donut_plot <- function(var_comps) {
+  df <- var_comps |>
+    dplyr::mutate(
+      fraction = percent / 100,
+      ymax = cumsum(fraction),
+      ymin = dplyr::lag(ymax, default = 0)
+    )
+
+  ggplot(df, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = component)) +
+    geom_rect() +
+    coord_polar(theta = "y") +
+    xlim(2, 4) +
+    geom_label(
+      aes(x = 3.5, y = (ymin + ymax) / 2, label = paste0(round(percent, 1), "%")),
+      fill = NA,label.size = 0
+    ) +
+    theme_void()
+}
+
+#' Plot variance components
+#' @noRd
+#' @keywords internal
+plot_var_comps <- function(var_comps, type = c("bar", "stacked", "pie", "donut")) {
+  type <- match.arg(type)
+
+  switch(type,
+         bar = bar_plot(var_comps),
+         stacked = stacked_bar_plot(var_comps),
+         pie = pie_plot(var_comps),
+         donut = donut_plot(var_comps)
+  )
+}
+
+#' @export
+#' @noRd
+#' @importFrom ggplot2 autoplot
 autoplot.asreml <- function(object, type = "bar", ...) {
   var_comps <- extract_var_comps.asreml(object)
   plot_var_comps(var_comps, type = type)
 }
 
-#' export
-autoplot.var_comps <- function(object, type = "bar", ...) {
-  plot_var_comps(object, type = type)
-}
+
--- tests/testthat/test-autoplot.R
+++ tests/testthat/test-autoplot.R
@@ -1,82 +1,16 @@
 test_that("Autoplot", {
-  library(tidyverse)
+  skip_if_not_installed("asreml")
+  skip_on_ci()
+  skip_on_cran()
 
   asreml_model_random <- readRDS(test_path("fixtures/asreml_model_random.rds"))
 
   # Extract the random effects
-  extract_var_comps.asreml <- function(model){
-    var_comps <- model$vparameters |> 
-    tibble() 
-    var_comps$component <- names(asreml_model_random$vparameters)
-    var_comps <- var_comps |> 
-    rename(value = 1) |> 
-    mutate(percent = value / sum(value) * 100)
-  }
+  expect_named(extract_var_comps(asreml_model_random))
 
-  var_comps <- extract_var_comps.asreml(asreml_model_random)
-
-  # Bar plot
-  bar_plot <- function(var_comps){
-  ggplot(var_comps, aes(x = component, y = value)) +
-    geom_bar(stat = "identity") +
-    labs(title = "Variance Components", x = "Component", y = "Value") +
-    theme_minimal()
-  }
-
-  # Stacked bar plot
-  stacked_bar_plot <- function(var_comps){
-  ggplot(var_comps, aes(x = "", y = value, fill = component)) +
-    geom_bar(stat = "identity", width = 1) +
-    geom_text(aes(label = paste0(round(percent, 1), "%")), 
-              position = position_stack(vjust = 0.5)) +
-    theme_minimal() +
-    theme(
-      axis.title = element_blank(),
-      axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        panel.grid  = element_blank()
-        ) 
-  }
-
-  # Pie chart
-  pie_plot <- function(var_comps){
-  ggplot(var_comps, aes(x = "", y = value, fill = component)) +
-    geom_bar(stat = "identity", width = 1) +
-    coord_polar("y", start = 0) +
-    geom_text(aes(label = paste0(round(percent, 1), "%")), 
-              position = position_stack(vjust = 0.5)) +
-    theme_minimal() +
-    theme(
-      axis.title = element_blank(),
-      axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        panel.grid  = element_blank()
-        )
-  }
-
-  # Donut chart
-  # Compute percentages
-  donut_plot <- function(var_comps){
-    var_comps <- var_comps |> 
-      mutate(fraction = percent/100,
-           ymax = cumsum(fraction), # Compute the cumulative percentages (top of each rectangle)
-           ymin = c(0, head(ymax, n=-1)))   # Compute the bottom of each rectangle
-  
-
-  ggplot(var_comps, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=component)) +
-      geom_rect() +
-      coord_polar(theta="y") + 
-      xlim(c(2, 4)) +
-      geom_label(aes(x=3.5, y=(ymin + ymax)/2, label=paste0(round(percent, 1), "%")), 
-             color="black", fill=NA, label.size=0) +
-      theme_minimal() +
-      theme(
-        axis.text = element_blank(),
-        axis.title = element_blank(),
-        axis.ticks = element_blank(),
-        panel.grid  = element_blank()
-        )
-        }
-
-  autoplot    
+  # Visual testing
+  vdiffr::expect_doppelganger("Bar plot", autoplot(asreml_model_random))
+  vdiffr::expect_doppelganger("Stacked bar plot", autoplot(asreml_model_random, type = "stacked"))
+  vdiffr::expect_doppelganger("Pie plot", autoplot(asreml_model_random, type = "pie"))
+  vdiffr::expect_doppelganger("Donut plot", autoplot(asreml_model_random, type = "donut"))
 })
