c5684a4a0e0e2d9cf14361fdf6c8bad1ef117797
Yidi Deng <meiosis97@gmail.com>
2026-03-09 21:28:36 +1100

    minor

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -18,30 +18,30 @@
 #     trace = FALSE,
 #   )
 #
-  model <- asreml(
-    fixed = y ~ rep,
-    random =  ~ loc + vm(gen, lettuce_GRM) + gen:loc ,
-    data = lettuce_phenotypes,
-    trace = FALSE,
-  )
-  var_comp(model, "gen", source = lettuce_GRM)
-  check_GRM_in_environment(model, "gen")
-  lettuce_GRM <- lettuce_GRM
-  H2_Cullis(model, "gen", source = lettuce_GRM)
-  H2_Cullis(model, "gen") # Beautiful
-  h2_Oakey(model, "gen")
-  h2(model,"gen")
-  h2_Delta_BLUP_pairwise.asreml(model, "gen", source = lettuce_GRM)
-  h2_Delta(model, "gen", source = lettuce_GRM)
-
-
-  lettuce_GRM <- lettuce_GRM
-  h2(model,"gen")
-  rm(lettuce_GRM)
-  h2(model,"gen")
-  h2(model, "gen", source = lettuce_GRM)
-
-
+#   model <- asreml(
+#     fixed = y ~ rep,
+#     random =  ~ loc + vm(gen, lettuce_GRM) + gen:loc ,
+#     data = lettuce_phenotypes,
+#     trace = FALSE,
+#   )
+#   var_comp(model, "gen", source = lettuce_GRM)
+#   check_GRM_in_environment(model, "gen")
+#   lettuce_GRM <- lettuce_GRM
+#   H2_Cullis(model, "gen", source = lettuce_GRM)
+#   H2_Cullis(model, "gen") # Beautiful
+#   h2_Oakey(model, "gen")
+#   h2(model,"gen")
+#   h2_Delta_BLUP_pairwise.asreml(model, "gen", source = lettuce_GRM)
+#   h2_Delta(model, "gen", source = lettuce_GRM)
+#
+#
+#   lettuce_GRM <- lettuce_GRM
+#   h2(model,"gen")
+#   rm(lettuce_GRM)
+#   h2(model,"gen")
+#   h2(model, "gen", source = lettuce_GRM)
+#
+#
 #
 #
 # })
